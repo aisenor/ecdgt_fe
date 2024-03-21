03d7eac98280c2c579ee7bd64d232e2d9b4e1d06
aisenor <aisenor@upei.ca>
2024-03-21 18:16:10 -0300

    added image to home page background, added social media icons to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,83 +1,103 @@
 import React, { useState } from 'react';
 import styles from './Contact.module.css';
-import MailIcon from './images/send_mail.png'
+import MailIcon from './images/send_mail.png';
+import TwitterIcon from './images/twitter.png';
+import FacebookIcon from './images/facebook.png';
+import InstagramIcon from './images/instagram.png';
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await fetch(`${process.env.REACT_APP_API_URL}/contact/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      alert('Failed to send email. Please try again later.');
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/contact/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+        } catch (error) {
+            console.error('Error sending email:', error);
+            alert('Failed to send email. Please try again later.');
+        }
+    };
 
-  return (
-      <div className={styles.container}>
-        <div className={styles.page}>
-          <h2>Contact Us</h2>
+    return (
+        <div className={styles.container}>
+            <div className={styles.page}>
+                <h2>Contact Us</h2>
+            </div>
+            {/* Social Media Links */}
+            <div className={styles.socialMediaContainer}>
+                <h3 className={styles.page}>Connect with us on social media!</h3>
+                <div className={styles.socialMediaIcons}>
+                    <a href="link-to-twitter-profile" target="_blank">
+                        <img src={TwitterIcon} alt="Twitter"/>
+                    </a>
+                    <a href="https://www.facebook.com/EastCoastDGT?locale=de_DE" target="_blank">
+                        <img src={FacebookIcon} alt="Facebook"/>
+                    </a>
+                    <a href="https://www.instagram.com/ec_dgt/" target="_blank">
+                        <img src={InstagramIcon} alt="Instagram"/>
+                    </a>
+                </div>
+            </div>
+
+            <div className={styles.formContainer}>
+                <div className={styles.contactFormImageContainer}>
+                    <img src={MailIcon} alt="mail"/>
+                </div>
+
+                <div className={styles.form}>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.inputGroup}>
+                            <label>Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label>Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label>Message</label>
+                            <textarea
+                                name="message"
+                                value={formData.message}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <br/>
+                        <button type="submit" className={styles.submitButton}>
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className={styles.formContainer}>
-          <div className={styles.contactFormImageContainer}>
-            <img src={MailIcon} alt="mail"/>
-          </div>
-
-          <div className={styles.form}>
-          <form onSubmit={handleSubmit}>
-              <div className={styles.inputGroup}>
-                <label>Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
-              </div>
-              <div className={styles.inputGroup}>
-                <label>Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-              </div>
-              <div className={styles.inputGroup}>
-                <label>Message</label>
-                <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                />
-              </div>
-              <br/>
-              <button type="submit" className={styles.submitButton}>Submit</button>
-            </form>
-          </div>
-        </div>
-
-      </div>
-  );
+    );
 };
 
 export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './Home.module.css'
 
 const Home = () => {
-  const [data, setData] = useState({});
 
-    useEffect(() => {
-      fetch(`${process.env.REACT_APP_API_URL}/`)
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.error('Error fetching data:', error));
-    }, []);
 
   return (
     <div className={styles.page}>
-      <h1>{data.message}</h1>
+      <h1>Welcome to the home of the East Coast Disc Golf Tour</h1>
+      <h3>Here you will find everything you need to know about the tour, how it started, and where it's going.</h3>
+      <p>Be sure to follow us on all social media platforms for up to date information on Tour Stops, Standings, and other cool things that are on their way.</p>
+      <p>Thank you for joining us, we can't wait to see you all on the course!</p>
     </div>
   );
 };
